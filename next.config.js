cda632b0e76a138425cade13ad5427c889431dc6
Jeff Li <jeff.li@autocase.com>
2021-11-24 22:22:50 -0500

    type checks with fork-ts-checker-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   async rewrites() {
     // released in Next.js 9.5: allows you to  map an incoming request path to a different 
     // destination path, including external URLs.
@@ -13,5 +14,18 @@ module.exports = {
         destination: '/',
       },
     ];
+  },
+  webpack: (config, { isServer }) => {
+    if(!isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+      config.plugins.push(new ForkTsCheckerWebpackPlugin({
+        eslint: {
+          files: './src/**/*.{ts,tsx,js,jsx}' // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+        }
+      }))
+    }
+    // Important: return the modified config
+    return config
   },
 }
